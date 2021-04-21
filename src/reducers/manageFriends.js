dc3201159c92c51aacf2bec483875f76b2fb5146
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-21 10:27:22 -0500

    commit tree d979848772b2535614df8f5417c8938907eefa7f

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,23 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return {
+        ...state,
+        friends: [...state.friends, action.friend],
+      };
+    case "REMOVE_FRIEND":
+      const removedFrom = state.friends.findIndex(
+        (friend) => friend.id === action.id
+      );
+
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, removedFrom),
+          ...state.friends.slice(removedFrom + 1),
+        ],
+      };
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
-export function managePresents(state, action){
+export function managePresents(state, action) {
+  state = { numberOfPresents: 0 };
+  switch (action.type) {
+    case "INCREASE":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
